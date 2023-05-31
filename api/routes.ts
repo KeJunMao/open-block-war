16f17447bd1e12f09cc163aee78daecb2549ec28
KeJunMao <kejun1997@gmail.com>
2023-05-31 12:00:17 +0800

    fix: 修复头像获取问题

--- api/routes.ts
+++ api/routes.ts
@@ -11,32 +11,33 @@ import GameHeartBeat from "./proxy/bilibiliServer/routes/gameHeartBeat"
 import GameBatchHeartBeat from "./proxy/bilibiliServer/routes/gameBatchHeartBeat"
 
 import start from "./proxy/start";
+import { getFaceByBilibili, getFaceByBilibili2, getFaceByTenApi } from "./proxy/getBilibiliFace";
 
 const router = Router();
 
-// router.get("/b/face/:id", async function (req, res) {
-//   const id = req.params.id;
-//   let face = "http://i0.hdslb.com/bfs/face/member/noface.jpg";
-//   try {
-//     face = await getFaceByBilibili(id);
-//   } catch (error) {
-//     try {
-//       face = await getFaceByBilibili2(id);
-//     } catch (error) {
-//       try {
-//         face = await getFaceByTenApi(id);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     }
-//   }
-//   if (!face) {
-//     face = "http://i0.hdslb.com/bfs/face/member/noface.jpg";
-//   }
-//   res.send({
-//     face,
-//   });
-// });
+router.get("/b/face/:id", async function (req, res) {
+  const id = req.params.id;
+  let face = "http://i0.hdslb.com/bfs/face/member/noface.jpg";
+  try {
+    face = await getFaceByBilibili(id);
+  } catch (error) {
+    try {
+      face = await getFaceByBilibili2(id);
+    } catch (error) {
+      try {
+        face = await getFaceByTenApi(id);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+  if (!face) {
+    face = "http://i0.hdslb.com/bfs/face/member/noface.jpg";
+  }
+  res.send({
+    face,
+  });
+});
 
 
 router.post("/b/getAuth", GetAuth)
